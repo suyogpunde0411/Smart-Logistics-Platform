766b51ce730ef45d8d13f36360ffe058c93d2d82
suyogpunde0411 <suyogpunde0411@gmail.com>
2026-07-18 23:39:26 +0530

    backend update

--- backend/tracking-service/target/generated-sources/annotations/com/smartlogistics/trackingservice/mapper/TripMapperImpl.java
+++ backend/tracking-service/target/generated-sources/annotations/com/smartlogistics/trackingservice/mapper/TripMapperImpl.java
@@ -27,7 +27,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-07-11T20:39:38+0530",
+    date = "2026-07-18T23:35:36+0530",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.46.100.v20260624-0231, environment: Java 21.0.11 (Eclipse Adoptium)"
 )
 @Component
